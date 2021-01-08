6ee96841c9625b3baf9660eb8fa6d7613891c3e0
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-08 16:37:20 -0700

    Finished through Table - employee, wanted to back up

--- index.sql
+++ index.sql
@@ -148,3 +148,26 @@ WHERE name LIKE '%Black%';
 -- Table - employee
 
 -- Problem 1
+SELECT first_name, last_name FROM employee
+WHERE city = 'Calgary';
+
+-- Problem 2
+SELECT MAX(birth_date) FROM employee;
+
+-- Problem 3
+SELECT MIN(birth_date) FROM employee;
+
+-- Problem 4
+SELECT * FROM employee
+WHERE reports_to = 2;
+
+-- Problem 5
+SELECT COUNT(*) FROM employee
+WHERE city = 'Lethbridge';
+
+-----------------------
+
+
+-- Table - invoice
+
+-- Problem 1
