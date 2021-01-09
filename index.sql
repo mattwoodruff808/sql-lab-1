fc01b00dd705a4d4ee5b4dbb150e8e5e5c6cdb11
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-08 20:15:07 -0700

    Finished the afternoon lab!

--- index.sql
+++ index.sql
@@ -171,3 +171,29 @@ WHERE city = 'Lethbridge';
 -- Table - invoice
 
 -- Problem 1
+SELECT COUNT(*) FROM invoice
+WHERE billing_country = 'USA';
+
+-- Problem 2
+SELECT MAX(total) FROM invoice;
+
+-- Problem 3
+SELECT MIN(total) FROM invoice;
+
+-- Problem 4
+SELECT * FROM invoice
+WHERE total > 5;
+
+-- Problem 5
+SELECT COUNT(*) FROM invoice
+WHERE total < 5;
+
+-- Problem 6
+SELECT COUNT(*) FROM invoice
+WHERE billing_state IN ('CA', 'TX', 'AZ');
+
+-- Problem 7
+SELECT AVG(total) FROM invoice;
+
+-- Problem 8
+SELECT SUM(total) FROM invoice;
